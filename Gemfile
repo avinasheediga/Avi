428210d4c9b178a3f41689b92906f54469b6c548
avinash <avinash@myedutor.com>
2014-03-13 05:49:02 +0530

    Fisrt Commit

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.0'
+gem 'sqlite3'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'therubyracer', platforms: :ruby
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+gem 'execjs'
+gem "paperclip", "~> 4.1"
+gem 'bootstrap-sass', '~> 3.1.1'
+
+group :doc do
+  gem 'sdoc', require: false
+  gem 'coffee-rails', '~> 4.0.0'
+  gem 'sass-rails', '~> 4.0.0'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Avi::Application.config.secret_key_base = '02c2107e4854e3dc359135b61a3761a0063bd72f718dedd6db8f204e7f7ed851578f04a25fc3304a281722ef355e5774d85fedcad46ddc7eddc15a2e67f42614'
